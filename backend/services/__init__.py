d3f661349bb0e70c0408cf741cceae7aa085bc31
Samuel Tech IA <samueldesignerpromax@gmail.com>
2026-03-23 18:36:23 -0300

    Create __init__.py

---
+++ backend/services/__init__.py
@@ -0,0 +1,5 @@
+from .chatbot import ChatbotIA
+from .pagamento import PagamentoService
+from .afiliado_service import AfiliadoService
+
+__all__ = ["ChatbotIA", "PagamentoService", "AfiliadoService"]
